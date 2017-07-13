bd9741c9801e13af581c283d134148398db030ad
axelinternet <axel@hultman.info>
2017-07-13 18:28:28 +0200

    Updated layout and readme

--- keyboard/keymap.c
+++ keyboard/keymap.c
@@ -7,23 +7,23 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          TAB,    Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P, LBRC, \
         CAPS,          A,    S,    D,    F,    G,    H,    J,    K,    L, SCLN, \
         LSFT,  FN1,    Z,    X,    C,    V,    B,    N,    M, COMM,  DOT, SLSH, \
-        LCTL, LGUI, LALT,                    SPC,              FN2,       RALT, \
-         EQL, RBRC, QUOT, RSFT, MENU, BSPC, BSLS,  ENT, RSFT, LCTL              ),
+        LCTL, LGUI, LALT,                    SPC,                    FN0, RALT, \
+         EQL, RBRC, QUOT, RSFT, RGUI, BSPC, BSLS,  ENT, RSFT, LCTL              ),
     /* layer 1 */
     KEYMAP(
          GRV,   F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,  F10,  F11, \
         TRNS, TRNS,   UP, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, PSCR, SLCK, PAUS, \
         TRNS,       LEFT, DOWN, RGHT, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, HOME, \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, VOLD, VOLU, MUTE, TRNS,  END, \
-        TRNS, TRNS, TRNS,                   TRNS,             TRNS,       TRNS, \
-         F12, TRNS, TRNS, TRNS, TRNS,  DEL, TRNS, TRNS, TRNS, TRNS              ),
+        TRNS, TRNS, TRNS,                   TRNS,                   TRNS, TRNS, \
+         F12, TRNS, TRNS, PGUP, TRNS,  DEL, TRNS, TRNS, PGDN, TRNS              ),
     /* layer 2 */
     KEYMAP(
         TRNS,   F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,  F10,  F11, \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,   UP, TRNS, TRNS, TRNS, \
         TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, LEFT, DOWN, RGHT, HOME, \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, VOLD, VOLU, MUTE, TRNS,  END, \
-        TRNS, TRNS, TRNS,                   TRNS,             TRNS,       TRNS, \
+        TRNS, TRNS, TRNS,                   TRNS,                   TRNS, TRNS, \
          F12, TRNS, TRNS, TRNS, TRNS,  DEL, TRNS, TRNS, TRNS, TRNS              ),
     /* layer 3 */
     KEYMAP(
@@ -31,15 +31,15 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TRNS, TRNS,   UP, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, PSCR, SLCK, PAUS, \
         TRNS,       LEFT, DOWN, RGHT, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, HOME, \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, VOLD, VOLU, MUTE, TRNS,  END, \
-        TRNS, TRNS, TRNS,                   TRNS,             TRNS,       TRNS, \
-         F12, TRNS, TRNS, TRNS, TRNS,  DEL, TRNS, TRNS, TRNS, TRNS              ),
+        TRNS, TRNS, TRNS,                   TRNS,                   TRNS, TRNS, \
+         F12, TRNS, TRNS, PGUP, TRNS,  DEL, TRNS, TRNS, PGDN, TRNS              ),
     /* layer 4 */
     KEYMAP(
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
         TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
-        TRNS, TRNS, TRNS,                   TRNS,             TRNS,       TRNS, \
+        TRNS, TRNS, TRNS,                   TRNS,                   TRNS, TRNS, \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS              ),
     /* layer 5 */
     KEYMAP(
@@ -47,7 +47,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
         TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
-        TRNS, TRNS, TRNS,                   TRNS,             TRNS,       TRNS, \
+        TRNS, TRNS, TRNS,                   TRNS,                   TRNS, TRNS, \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS              ),
     /* layer 6 */
     KEYMAP(
@@ -55,7 +55,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
         TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
-        TRNS, TRNS, TRNS,                   TRNS,             TRNS,       TRNS, \
+        TRNS, TRNS, TRNS,                   TRNS,                   TRNS, TRNS, \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS              ),
     /* layer 7 */
     KEYMAP(
@@ -63,7 +63,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
         TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
-        TRNS, TRNS, TRNS,                   TRNS,             TRNS,       TRNS, \
+        TRNS, TRNS, TRNS,                   TRNS,                   TRNS, TRNS, \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS              ),
 };
 
